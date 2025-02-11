1620931318e0c19dfbe72e1e95580e99698db24d
notepadguyOfficial <rerollhashtagnumber1@gmail.com>
2025-02-11 16:06:15 +0800

    Add initial setup for Azerothcore with Docker and environment configuration

--- README.md
+++ README.md
@@ -1,2 +1,154 @@
-# WSL-Debian-Docker-Azerothcore-Playerbots-Installation
-A Compilation Guide of Installing Azerothcore on WSL (Debian) with Docker Containers
+# WSL Debian Docker Azerothcore-Playerbots Installation
+
+## Table of Contents
+
+- [Requirements](#requirements)
+- [Git Cloning](#gitcloning)
+- [WSL Installation](#wslinstallation)
+- [Docker Installation](#dockerinstallation)
+- [Azerothcore-Playerbots Introduction](#azerothcore-playerbots)
+- [AzerothCore Bot Installation](#azerothcorebotinstallation)
+
+## Requirements
+ - wsl2
+ 
+## Git Cloning
+1. Clone Repository:
+	```cmd
+	git clone https://github.com/notepadguyOfficial/Debian-Docker-Azerothcore-Playerbot.git
+	```
+ 
+## WSL Installation
+1. Open cmd/Powershell and run this command:
+	```cmd
+	wsl --insall -d Debian
+	```
+
+2. Once Installed, Export the a backup to the desired location:
+	```cmd
+	wsl --export Debian C:\Example
+	```
+	or
+	```cmd
+	wsl --export Debian D:\Example
+	```
+
+3. Unregister the Original Debian Image from WSL:
+	```cmd
+	wsl --unregister Debian
+	```
+
+4. Import it back into your chosen location:
+	```cmd
+	wsl --import Debian C:\Debian C:\Example\Debian.tar --version 2
+	```
+	or
+	```cmd
+	wsl --import Debian D:\Debian D:\Example\Debian.tar --version 2
+	```
+	
+ - (Optional) Delete the .tar file to free space:
+	```cmd
+	Remove-Item D:\Example\Debian.tar
+	```
+5. Launch Debian:
+	```cmd
+	wsl -d Debian
+	```
+
+## Docker Installation
+1. see [Git Cloning](#gitcloning)
+
+2. Run WSL via cmd/Powershell:
+    ```cmd
+    wsl -d Debian
+    ```
+
+3. Navigate to repository:
+    ```cmd
+    cd /mnt/c/to-your-clones-repository-directory
+    ```
+    or if you cloned the repository in another drive:
+	```cmd
+    cd /mnt/d/to-your-clones-repository-directory
+    ```
+
+4. run the sh file:
+	```cmd
+	./setup-docker-wsl.sh
+	```
+
+ - (!Note) If you run the script but with error, This usually means Windows-style line endings instead of the Unix-style.
+	To fix this run this command
+	```cmd
+	sudo apt install dos2unix
+
+	#then
+
+	dos2unix *.sh
+
+	#final command
+
+	chmod +x *.sh
+	```
+
+5. wait until done then restart wsl:
+	```cmd
+	wsl --shutdown
+	```
+	then start the wsl service again
+	```
+	wsl -d Debian
+	```
+	
+## Azerothcore-Playerbot
+Includes:
+ - MariaDB-Client (This is only client and will install only if you dont have the mysql command...)
+ - Docker (Will install if Docker is not installed.)
+ - Azeroth Core - Playerbots branch
+ - mod-playerbots
+ - mod-aoe-loot (optional)
+ - mod-learn-spells (optional)
+ - mod-fireworks-on-level (optional)
+ - mod-individual-progression (optional)
+
+### Update
+ - To update and get the latest versions, you can run `./uninstall.sh` without deleting the volumes and run `./setup.sh` again. It will prompt you if you want to delete the volumes.
+
+ - You can add modules to the `setup.sh` file by scrolling to the "install_mod" section and add the entries you'd like. Or you could do it manually by putting the modules folders into the `azerothcore-wotlk/modules` folder. `setup.sh` will automatically add the sql. See
+
+ - Running `setup.sh` will not install anything over again unless you delete a modules folder or the `azerothcore-wotlk` folder before. You can run it if you only want to install new modules youve added, it will skip if you already downloaded the repos. 
+
+- If you delete modules, remember to run `clear_custom_sql.sh` first and remove the respective tables in the db.
+
+## AzerothCore Bot Installation
+1. Run Setup sh Script:
+	```cmd
+	./setup.sh
+	```
+
+2. choose if you want to install modules besides the given one.
+ - (Optional) You can edit `setup.sh` to add another modules
+ 
+3. Wait for docker to install the images.
+
+4. If no error then attach to ac-worldserver by running this command:
+	```cmd
+	docker attach ac-worldserver
+	```
+	(!Note) You can dettach from attached service by pressing `ctrl+p` and `ctrl+q`.
+	
+5. Create Azerothcore Account
+	```cmd
+	account create <username> <password>
+	```
+	
+6. Find WSL IP Address
+	```cmd
+	ip addr show eth0
+	```
+	(!Important) when connecting to database or game use the wsl ip address.
+	
+7. Edit your `<your wow directory root>\Data\enUS\realmlist.wtf` and type in the ip address you get in the end of installing..." set realmlist dockerhost_ip 192.168.48.2 "
+
+8. Run the Game and your done.
---
+++ setup.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+function ask_user() {
+    read -p "$1 (y/n): " choice
+    case "$choice" in
+        y|Y ) return 0;;
+        * ) return 1;;
+    esac
+}
+
+sed -i "s|^TZ=.*$|TZ=$(cat /etc/timezone)|" src/.env
+
+sudo apt update
+
+if ! command -v mysql &> /dev/null
+then
+    echo "MySQL client is not installed. Installing mariadb-client now..."
+    sudo apt install -y mariadb-client
+else
+    echo "MySQL client is already installed."
+fi
+
+if ! command -v docker &> /dev/null
+then
+    echo "Docker is not installed. Installing Docker now..."
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo usermod -aG docker $USER
+    echo "::NOTE::"
+    echo "Added your user to docker group to manage docker without root."
+    echo "Log out and back in and rerun setup.sh."
+    exit 1;
+else
+    echo "Docker is already installed."
+fi
+
+if [ -d "azerothcore-wotlk" ]; then
+    destination_dir="data/sql/custom"
+    
+    world=$destination_dir"/db_world/"
+    chars=$destination_dir"/db_characters/"
+    auth=$destination_dir"/db_auth/"
+    
+    cd azerothcore-wotlk
+    
+    rm -rf $world/*.sql
+    rm -rf $chars/*.sql
+    rm -rf $auth/*.sql
+    
+    cd ..
+    
+    cp src/.env azerothcore-wotlk/
+    cp src/*.yml azerothcore-wotlk/
+    cd azerothcore-wotlk
+else
+    if ask_user "Download and install AzerothCore Playerbots?"; then
+        git clone https://github.com/liyunfan1223/azerothcore-wotlk.git --branch=Playerbot
+        cp src/.env azerothcore-wotlk/
+        cp src/*.yml azerothcore-wotlk/
+        cd azerothcore-wotlk/modules
+        git clone https://github.com/liyunfan1223/mod-playerbots.git --branch=master
+        cd ..
+    else
+        echo "Aborting..."
+        exit 1
+    fi
+fi
+
+if ask_user "Install modules?"; then
+
+    cd modules
+
+    function install_mod() {
+    local mod_name=$1
+    local repo_url=$2
+
+    if [ -d "${mod_name}" ]; then
+        echo "${mod_name} exists. Skipping..."
+    else
+        if ask_user "Install ${mod_name}?"; then
+            git clone "${repo_url}"
+        fi
+    fi
+    }
+
+    install_mod "mod-aoe-loot" "https://github.com/azerothcore/mod-aoe-loot.git"
+    install_mod "mod-learn-spells" "https://github.com/noisiver/mod-learnspells.git"
+    install_mod "mod-fireworks-on-level" "https://github.com/azerothcore/mod-fireworks-on-level.git"
+    install_mod "mod-individual-progression" "https://github.com/ZhengPeiRu21/mod-individual-progression.git"
+    install_mod "mod-junk-to-gold" "https://github.com/notepadguyOfficial/mod-junk-to-gold.git"
+    install_mod "mod-dungeon-respawn" "https://github.com/notepadguyOfficial/DungeonRespawn.git"
+    cd ..
+
+fi
+
+
+docker compose up -d --build
+
+cd ..
+
+sudo chown -R 1000:1000 wotlk
+
+custom_sql_dir="src/sql"
+auth="acore_auth"
+world="acore_world"
+chars="acore_characters"
+
+ip_address=$(hostname -I | awk '{print $1}')
+
+temp_sql_file="/tmp/temp_custom_sql.sql"
+
+function execute_sql() {
+    local db_name=$1
+    local sql_files=("$custom_sql_dir/$db_name"/*.sql)
+
+    if [ -e "${sql_files[0]}" ]; then
+        for custom_sql_file in "${sql_files[@]}"; do
+            echo "Executing $custom_sql_file"
+            temp_sql_file=$(mktemp)
+            if [[ "$(basename "$custom_sql_file")" == "update_realmlist.sql" ]]; then
+                sed -e "s/{{IP_ADDRESS}}/$ip_address/g" "$custom_sql_file" > "$temp_sql_file"
+            else
+                cp "$custom_sql_file" "$temp_sql_file"
+            fi
+            mysql -h "$ip_address" -uroot -ppassword "$db_name" < "$temp_sql_file"
+        done
+    else
+        echo "No SQL files found in $custom_sql_dir/$db_name, skipping..."
+    fi
+}
+
+echo "Running custom SQL files..."
+execute_sql "$auth"
+execute_sql "$world"
+execute_sql "$chars"
+
+rm "$temp_sql_file"
+
+echo ""
+echo "NOTE:"
+echo ""
+echo "!!! If ac-db-import failed, run 'sudo chown -R 1000:1000 wotlk' and './setup.sh' again !!!"
+echo ""
+echo "1. Execute 'docker attach ac-worldserver'"
+echo "2. 'account create username password' creates an account."
+echo "3. 'account set gmlevel username 3 -1' sets the account as gm for all servers."
+echo "4. Ctrl+p Ctrl+q will take you out of the world console."
+echo "5. Edit your gameclients realmlist.wtf and set it to $ip_address."
+echo "6. Now login to wow with 3.3.5a client!"
+echo "7. All config files are copied into the wotlk folder created with setup.sh."
